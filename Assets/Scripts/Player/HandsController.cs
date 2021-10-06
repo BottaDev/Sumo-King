98324f331e652880b060eea53b7109d4dab6a060
Federico Botta <fedebotta@gmail.com>
2021-10-06 02:03:56 -0300

    Minor changes

--- Assets/Scripts/Player/HandsController.cs
+++ Assets/Scripts/Player/HandsController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class HandsController : MonoBehaviourPun
@@ -8,10 +9,17 @@ public class HandsController : MonoBehaviourPun
     public PlayerState state;
     public Animation anim;
 
+    private GameObject _bodyParty;
+    
     private void ApplyForceOnBody(GameObject body)
     {
-        Rigidbody rb = body.gameObject.GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward * state.pushForce, ForceMode.Impulse);
+        if (!photonView.IsMine) 
+            return;
+
+        _bodyParty = body;
+        
+        Player player = body.GetComponentInParent<PhotonView>().Owner;
+        photonView.RPC("ApplyForceOnBodyRPC", player);
 
         state.ApplyDebuff();
         anim.Play();
@@ -19,20 +27,39 @@ public class HandsController : MonoBehaviourPun
 
     private void ApplyForceOnHands(GameObject hands)
     {
-        Rigidbody rb = hands.gameObject.GetComponentInParent<Rigidbody>();
-        rb.AddForce(transform.forward * (state.pushForce / 2), ForceMode.Impulse);
+        if (!photonView.IsMine) 
+            return;
+
+        _bodyParty = hands;
+        
+        Player player = hands.GetComponentInParent<PhotonView>().Owner;
+        photonView.RPC("ApplyForceOnHandsRPC", player);
 
         state.ApplyDebuff();
         anim.Play();
     }
 
-    private void OnTriggerEnter(Collider collider)
+    [PunRPC]
+    private void ApplyForceOnHandsRPC()
     {
-        if (collider.gameObject.CompareTag("Player"))
-            ApplyForceOnBody(collider.gameObject);
+        Rigidbody rb = _bodyParty.gameObject.GetComponentInParent<Rigidbody>();
+        rb.AddForce(transform.forward * (state.pushForce / 2), ForceMode.Impulse);
+    }
+    
+    [PunRPC]
+    private void ApplyForceOnBodyRPC()
+    {
+        Rigidbody rb = _bodyParty.gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * state.pushForce, ForceMode.Impulse);
+    }
+    
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+            ApplyForceOnBody(col.gameObject);
 
-        if (collider.gameObject.CompareTag("Hands"))
-            ApplyForceOnHands(collider.gameObject);
+        if (col.gameObject.CompareTag("Hands"))
+            ApplyForceOnHands(col.gameObject);
         
     }
 }
